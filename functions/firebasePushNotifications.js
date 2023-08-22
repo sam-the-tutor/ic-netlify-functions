af4032269b76775835252301b1ae9263b69ba241
sam-the-tutor <atwebembeiresamuel2020@gmail.com>
2023-08-22 14:19:58 +0300

    hj

--- functions/firebasePushNotifications.js
+++ functions/firebasePushNotifications.js
@@ -10,11 +10,14 @@ exports.handler = async function (event, context) {
   }
   const to =
     "ewYwhLDp0P_9H9U2EEFMqJ:APA91bFdimxPX_wxg6X-WWtSd3hZ5M990wURboxQkoE04vk8jisy-JVYInw7Kflljehgvet0fdMdI6jS-BoVEPdDBWxQ7FhdIu7-o1TraQjmKKkVAApGbTC34ZznAxE54Sg23cBFeNnm"
+  const fromo =
+    "ewYwhLDp0P_9H9U2EEFMqJ:APA91bFdimxPX_wxg6X-WWtSd3hZ5M990wURboxQkoE04vk8jisy-JVYInw7Kflljehgvet0fdMdI6jS-BoVEPdDBWxQ7FhdIu7-o1TraQjmKKkVAApGbTC34ZznAxE54Sg23cBFeNnm"
+
   try {
     const response = fetch("https://fcm.googleapis.com/fcm/send", {
       method: "POST",
       headers: {
-        Authorization: "key=" + process.env.FIREBAASE_SERVER_KEY,
+        Authorization: "key=" + process.env.FIREBASE_SERVER_KEY,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
@@ -23,11 +26,11 @@ exports.handler = async function (event, context) {
       }),
     })
 
-    const responseBody = (await response).json()
+    const responseBody = await response.json()
 
     return {
       statusCode: 200,
-      body: JSON.stringify(response),
+      body: JSON.stringify(responseBody),
     }
   } catch (err) {
     console.log(err)
