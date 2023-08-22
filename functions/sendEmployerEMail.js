09666159220ef26d5c0d103ad41d3f89bbf4fcc3
sam-the-tutor <atwebembeiresamuel2020@gmail.com>
2023-08-22 11:59:57 +0300

    added sendEmployerEmail function

---
+++ functions/sendEmployerEMail.js
@@ -0,0 +1,50 @@
+exports.handler = async function (event, context) {
+  //Needs to recieve the job_name,employee_name and to_name from the canister in the event..
+  const myData = JSON.parse(event.body)
+
+  //construct the data in a format needed by the emailjs to send an email
+  var data = {
+    service_id: process.env.SERVICE_ID,
+    template_id: process.env.EMPLOYER_TEMPLATE_ID,
+    user_id: process.env.USER_ID,
+    template_params: {
+      job_name: myData.job_name,
+      employer_email: myData.to_name,
+      applicant_link: myData.employee_email,
+    },
+  }
+
+  try {
+    //execute the fetch function to the api
+    const response = await fetch(
+      "https://api.emailjs.com/api/v1.0/email/send",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    )
+    let responseText = await response.text()
+    if (responseText === "OK") {
+      return {
+        statusCode: 200,
+        body: responseText,
+      }
+    } else {
+      return {
+        statusCode: 400,
+        body: JSON.stringify(
+          "Email not sent. Unknown error.Please try again later"
+        ),
+      }
+    }
+    //catch any errors and return them to the user
+  } catch (e) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify(e),
+    }
+  }
+}
