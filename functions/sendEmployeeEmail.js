ae97448c11ae47fe4d558e05af29ff862f044fe5
sam-the-tutor <atwebembeiresamuel2020@gmail.com>
2023-08-22 11:17:38 +0300

    added comments

--- functions/sendEmployeeEmail.js
+++ functions/sendEmployeeEmail.js
@@ -1,7 +1,8 @@
 exports.handler = async function (event, context) {
-  const myData = JSON.parse(event.body)
   //Needs to recieve the job_name,employee_name and to_name from the canister in the event..
+  const myData = JSON.parse(event.body)
 
+  //construct the data in a format needed by the emailjs to send an email
   var data = {
     service_id: process.env.SERVICE_ID,
     template_id: process.env.EMPLOYEE_TEMPLATE_ID,
@@ -12,9 +13,9 @@ exports.handler = async function (event, context) {
       employee_email: myData.employee_email,
     },
   }
-  //fetch('www.example.com/submit-form', { method: 'POST', // Specify the HTTP method body: new FormData(document.querySelector('form')) // Collect form data }) .then(response => response.text()) // Read response as text .then(data => alert(data)) // Alert the response
 
   try {
+    //execute the fetch function to the api
     const response = await fetch(
       "https://api.emailjs.com/api/v1.0/email/send",
       {
@@ -39,11 +40,7 @@ exports.handler = async function (event, context) {
         ),
       }
     }
-
-    // return {
-    //   statusCode: 200,
-    //   body: JSON.stringify(myData.name),
-    // }
+    //catch any errors and return them to the user
   } catch (e) {
     return {
       statusCode: 404,
