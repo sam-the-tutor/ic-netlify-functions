7796139068224e3a075615c584ed814ae462744a
sam-the-tutor <atwebembeiresamuel2020@gmail.com>
2023-08-22 14:23:35 +0300

    H

--- functions/firebasePushNotifications.js
+++ functions/firebasePushNotifications.js
@@ -10,14 +10,14 @@ exports.handler = async function (event, context) {
   }
   const to =
     "ewYwhLDp0P_9H9U2EEFMqJ:APA91bFdimxPX_wxg6X-WWtSd3hZ5M990wURboxQkoE04vk8jisy-JVYInw7Kflljehgvet0fdMdI6jS-BoVEPdDBWxQ7FhdIu7-o1TraQjmKKkVAApGbTC34ZznAxE54Sg23cBFeNnm"
-  const fromo =
-    "ewYwhLDp0P_9H9U2EEFMqJ:APA91bFdimxPX_wxg6X-WWtSd3hZ5M990wURboxQkoE04vk8jisy-JVYInw7Kflljehgvet0fdMdI6jS-BoVEPdDBWxQ7FhdIu7-o1TraQjmKKkVAApGbTC34ZznAxE54Sg23cBFeNnm"
+  const key =
+    "AAAAgEy3QBw:APA91bEHp0aJ0OnC4YXfXgxPi2jmefXrqnYPGOpwsOEY27ARkztGycfUEnCs0GDcQ-qAXk5govzPLXUJluAtCwIZ-StTQDT8tGdaFAw7s3cuuy4IFBsCtXtekaYWzv9YzOsyLS6X2mFb"
 
   try {
     const response = fetch("https://fcm.googleapis.com/fcm/send", {
       method: "POST",
       headers: {
-        Authorization: "key=" + process.env.FIREBASE_SERVER_KEY,
+        Authorization: "key=" + key,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
