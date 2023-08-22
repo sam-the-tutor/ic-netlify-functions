b5a8850559998d1930f7e043f459c4682c4397d3
sam-the-tutor <atwebembeiresamuel2020@gmail.com>
2023-08-22 03:16:22 +0300

    more work on sendEmployeeEmail

---
+++ functions/sendEmployeeEmail.js
@@ -0,0 +1,38 @@
+exports.handler = async function (event, context) {
+  //Needs to recieve the job_name,employee_name and to_name from the canister in the event..
+
+  var data = {
+    service_id: process.env.SERVICE_ID,
+    template_id: process.env.EMPLOYEE_TEMPLATE_ID,
+    user_id: process.env.USER_ID,
+    template_params: {
+      job_name: "Software Engineer",
+      to_name: "Paul",
+      employee_email: "peacevian66@gmail.com",
+    },
+  }
+  //fetch('www.example.com/submit-form', { method: 'POST', // Specify the HTTP method body: new FormData(document.querySelector('form')) // Collect form data }) .then(response => response.text()) // Read response as text .then(data => alert(data)) // Alert the response
+
+  try {
+    const response = await fetch(
+      "https://api.emailjs.com/api/v1.0/email/send",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    )
+    let responseText = await response.text()
+    return {
+      statusCode: 200,
+      body: responseText,
+    }
+  } catch (e) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify(e),
+    }
+  }
+}
