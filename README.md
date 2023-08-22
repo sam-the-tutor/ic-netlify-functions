54d7e0dc889feda4b6c4d03a8f364b95ab8f6ce8
sam-the-tutor <atwebembeiresamuel2020@gmail.com>
2023-08-22 13:50:27 +0300

    added firebasePushNotifications

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ three files that are included
 
 * `sendEmployerEMail.js` - this sends the employer an email once the uemployee applies for their job
 * `sendNotification.js` - this sends a push notification to the employer's phone notifying them of the new application on their posted job
+
+exLwpsnkBjWEuUlnjwmwci:APA91bHDRd_HSy5E2B0Lw40TFyyPSEKr9ovA2r5cn5pp9_IiGUb81Tl_cRYrIp2FvcIyFN9EiNjtMWe-WXVjq8ApvA2MBgMPQ0YqGc0zDwLA1ZgDoaSlLcwpgC0EqhY-8TdwwsxXSVzb
+
+ewYwhLDp0P_9H9U2EEFMqJ:APA91bFdimxPX_wxg6X-WWtSd3hZ5M990wURboxQkoE04vk8jisy-JVYInw7Kflljehgvet0fdMdI6jS-BoVEPdDBWxQ7FhdIu7-o1TraQjmKKkVAApGbTC34ZznAxE54Sg23cBFeNnm
---
+++ functions/firebasePushNotifications.js
@@ -0,0 +1,37 @@
+exports.handler = async function (event, context) {
+  const myData = JSON.parse(event.body)
+
+  var notification = {
+    title: "New Job Application",
+
+    body: `You have recieved a new job application for the ${myData.job_name}`,
+    icon: "ic-logo.jpg",
+    click_action: myData.applicant_link,
+  }
+  try {
+    const response = fetch("https://fcm.googleapis.com/fcm/send", {
+      method: "POST",
+      headers: {
+        Authorization: "key=" + process.env.FIREBAASE_SERVER_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        notification: notification,
+        to: myData.phone_code,
+      }),
+    })
+
+    const responseBody = (await response).json()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify("working"),
+    }
+  } catch (err) {
+    console.log(err)
+    return {
+      statusCode: 404,
+      body: JSON.stringify(err),
+    }
+  }
+}
