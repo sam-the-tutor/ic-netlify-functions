b0e516a7b5a0ef869da91a257dd2042f5827b74d
Sam-the-tutor <atwebembeiresamuel2020@gmail.com>
2023-10-20 04:20:57 +0300

    d

--- functions/icpEmailApp.js
+++ functions/icpEmailApp.js
@@ -1,41 +1,46 @@
 //send a specific email to the user..
 exports.handler = async function (event, context) {
-  const myData = JSON.parse(event.body);
+  //   const myData = JSON.parse(event.body);
 
-  const data = {
-    service_id: "service_0ktpvva",
-    template_id: "template_5ex8f4c",
-    user_id: "PRgnEfVQsYJOuZasb",
-    template_params: {
-      recipient_email: myData.recipientEmail,
-      to_name: myData.toName,
-    },
-  };
+  //   const data = {
+  //     service_id: "service_0ktpvva",
+  //     template_id: "template_5ex8f4c",
+  //     user_id: "PRgnEfVQsYJOuZasb",
+  //     template_params: {
+  //       recipient_email: myData.recipientEmail,
+  //       to_name: myData.toName,
+  //     },
+  //   };
 
   try {
-    //execute the fetch function to the api
-    const response = await fetch(
-      "https://api.emailjs.com/api/v1.0/email/send",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    );
-    let responseText = await response.text();
-    if (responseText === "OK") {
-      return {
-        statusCode: 200,
-        body: responseText,
-      };
-    } else {
-      return {
-        statusCode: 400,
-        body: JSON.stringify(responseText),
-      };
-    }
+    // //execute the fetch function to the api
+    // const response = await fetch(
+    //   "https://api.emailjs.com/api/v1.0/email/send",
+    //   {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(data),
+    //   }
+    // );
+    // let responseText = await response.text();
+    // if (responseText === "OK") {
+    //   return {
+    //     statusCode: 200,
+    //     body: responseText,
+    //   };
+    // } else {
+    //   return {
+    //     statusCode: 400,
+    //     body: JSON.stringify(responseText),
+    //   };
+    // }
+
+    return {
+      statusCode: 200,
+      body: responseText,
+    };
     //catch any errors and return them to the user
   } catch (e) {
     return {
