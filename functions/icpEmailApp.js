2bba496220436e41b1c69b9940ebba55cc67d6f4
Sam-the-tutor <atwebembeiresamuel2020@gmail.com>
2023-10-20 04:50:12 +0300

    modified the icEmailApp

--- functions/icpEmailApp.js
+++ functions/icpEmailApp.js
@@ -19,34 +19,34 @@ exports.handler = async function (event, context) {
   };
 
   try {
-    // //execute the fetch function to the api
-    // const response = await fetch(
-    //   "https://api.emailjs.com/api/v1.0/email/send",
-    //   {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(data),
-    //   }
-    // );
-    // let responseText = await response.text();
-    // if (responseText === "OK") {
-    //   return {
-    //     statusCode: 200,
-    //     body: responseText,
-    //   };
-    // } else {
-    //   return {
-    //     statusCode: 400,
-    //     body: JSON.stringify(responseText),
-    //   };
-    // }
+    //execute the fetch function to the api
+    const response = await fetch(
+      "https://api.emailjs.com/api/v1.0/email/send",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    let responseText = await response.text();
+    if (responseText === "OK") {
+      return {
+        statusCode: 200,
+        body: responseText,
+      };
+    } else {
+      return {
+        statusCode: 400,
+        body: JSON.stringify(responseText),
+      };
+    }
 
-    return {
-      statusCode: 200,
-      body: responseText,
-    };
+    // return {
+    //   statusCode: 200,
+    //   body: responseText,
+    // };
     //catch any errors and return them to the user
   } catch (e) {
     return {
