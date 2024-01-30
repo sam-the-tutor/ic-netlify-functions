c7ce18537b729cc3eb4a833febb2b59454963f0d
Sam-the-tutor <smartskillsweb3@gmail.com>
2024-01-30 15:55:32 +0300

    added user id

--- functions/taggrBotNotification.js
+++ functions/taggrBotNotification.js
@@ -7,7 +7,7 @@ exports.handler = async function (event, context) {
   var data = {
     service_id: "service_v46belc",
     template_id: "template_83d4t4k",
-    user_id: process.env.USER_ID,
+    user_id: "PRgnEfVQsYJOuZasb",
     template_params: {
       principal_id: myData.principal_id,
       media_account: myData.media_account,
